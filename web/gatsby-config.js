d2e67c32222c74928087ebd913c3493c78bd3af7
flaturtha <rich@talesofmurder.com>
2020-03-20 15:56:32 -0500

    adds Linaria, TailwindCSS ... not working, however!

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -10,6 +10,7 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   plugins: [
     'gatsby-plugin-postcss',
+    'gatsby-plugin-linaria',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-sanity',
--- web/src/pages/scripts.js
+++ web/src/pages/scripts.js
@@ -3,11 +3,16 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+import '../styles/tailwind.css'
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title='scripts' />
-    <h1>SCRIPTS</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness. ;-(</p>
+    <div className="container mx-auto">
+      <h1 className="text-orange-500">SCRIPTS</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness. ;-(</p>
+    </div>
+    
   </Layout>
 )
 
